5a3662fc57704dfe31e43fed25b09976a034dafd
ahunter623 <ahunter623@gmail.com>
2023-09-28 15:30:26 -0400

    cards are back

--- script.js
+++ script.js
@@ -4,24 +4,27 @@ const playerLives = 5;
 
 playerLivesCount.textContent = playerLives
 
+// Generate Object
 const getData = () => [
-    {imgSrc: "./Dark-Side-Images/Darth Vader Kneel1.jpeg"},
-    {imgSrc: "./Dark-Side-Images/Kylo Ren.jpeg"},
-    {imgSrc: "./Dark-Side-Images/Darth Maul Hood.jpeg"},
-    {imgSrc: "./Dark-Side-Images/Darth Maul Light Saber.jpeg"},
-    {imgSrc: "./Dark-Side-Images/Darth Vader Grab.webp"},
-    {imgSrc: "./Dark-Side-Images/Emperor Palpatine.jpeg"},
-    {imgSrc: "./Dark-Side-Images/Kylo Ren2.jpeg"},
-    {imgSrc: "./Dark-Side-Images/Palpatine_FandD.webp"},
-    {imgSrc: "./Jedi-Images/Ahsoka Tano Light Saber.jpeg"},
-    {imgSrc: "./Jedi-Images/Ahsoka-Tano-lightsabers-pose-df36cb5.jpeg"},
-    {imgSrc: "./Jedi-Images/Grogu.jpeg"},
-    {imgSrc: "./Jedi-Images/Grogu2.jpeg"},
-    {imgSrc: "./Jedi-Images/Luke Skywalker - Return of the Jedi.jpeg"},
-    {imgSrc: "./Jedi-Images/luke_the_hero_small.webp"},
-    {imgSrc: "./Jedi-Images/Obi Wan.webp"},
-    {imgSrc: "./Jedi-Images/obi-wan-kenobi-headline.webp"},
-]
+    {imgSrc: "./Dark-Side-Images/Darth Vader Kneel1.jpeg", name: "Darth Vader Kneel"},
+    {imgSrc: "./Dark-Side-Images/Kylo Ren.jpeg", name: "Kylo Ren"},
+    {imgSrc: "./Dark-Side-Images/Darth Maul Hood.jpeg", name: "Darth Maul Hood"},
+    {imgSrc: "./Dark-Side-Images/Darth Maul Light Saber.jpeg", name: "Darth Maul Light Saber"},
+    {imgSrc: "./Dark-Side-Images/Darth Vader Grab.webp", name: "Darth Vader Grab"},
+    {imgSrc: "./Dark-Side-Images/Emperor Palpatine.jpeg", name: "Emperor Palpatine"},
+    {imgSrc: "./Dark-Side-Images/Kylo Ren2.jpeg", name: "Kylo Ren2"},
+    {imgSrc: "./Dark-Side-Images/Palpatine_FandD.webp", name: "Palpatine FandD"},
+    {imgSrc: "./Dark-Side-Images/Darth Vader Kneel1.jpeg", name: "Darth Vader Kneel"},
+    {imgSrc: "./Dark-Side-Images/Kylo Ren.jpeg", name: "Kylo Ren"},
+    {imgSrc: "./Dark-Side-Images/Darth Maul Hood.jpeg", name: "Darth Maul Hood"},
+    {imgSrc: "./Dark-Side-Images/Darth Maul Light Saber.jpeg", name: "Darth Maul Light Saber"},
+    {imgSrc: "./Dark-Side-Images/Darth Vader Grab.webp", name: "Darth Vader Grab"},
+    {imgSrc: "./Dark-Side-Images/Emperor Palpatine.jpeg", name: "Emperor Palpatine"},
+    {imgSrc: "./Dark-Side-Images/Kylo Ren2.jpeg", name: "Kylo Ren2"},
+    {imgSrc: "./Dark-Side-Images/Palpatine_FandD.webp", name: "Palpatine FandD"},
+    //{imgSrc: "./Dark-Side-Images/Galactic Empire Symbol.jpeg"},
+    //{imgSrc: "./Jedi-Images/Jedi Symbol.jpeg"},
+];
 
 const randomize = () => {
     const cardData = getData()
@@ -30,13 +33,12 @@ const randomize = () => {
 }
 
 const cardGenerator = () => {
-    const cardData = randomize()
+    const cardData = randomize();
     console.log(cardData)
 
+    //Generate HTML
     cardData.forEach((item) => {
-        console.log(item)
-     
-
+        
     const card = document.createElement("div")
     const face = document.createElement('img')
     const back = document.createElement("div")
@@ -44,23 +46,47 @@ const cardGenerator = () => {
     face.classList = "face"
     back.classList = "back"
 
+    //Attach Images to Cards
     face.src = item.imgSrc
+    card.setAttribute("name", item.name);
 
+    //Attach Cards
     section.appendChild(card)
     card.appendChild(face)
     card.appendChild(back)
 
-    card.addEventListener('click', (e) => {
+    card.addEventListener("click", (e) => {
         card.classList.toggle("toggleCard");
         checkCards(e);
     })
 })
 
+//Check Cards
 const checkCards = (e) => {
+    console.log(e)
     const clickedCard = e.target;
-    console.log(clickedCard)
-}
-}
+    clickedCard.classList.add("flipped");
+    const flippedCards = document.querySelectorAll(".flipped")
+    console.log(flippedCards);
 
-
-cardGenerator();
+    // Game Logic
+if (flippedCards.length === 2) {
+    if (flippedCards[0].getAttribute("name") === 
+        flippedCards[1].getAttribute("name")
+    ) {
+    console.log("match");
+    flippedCards.forEach(card => {
+        card.classList.remove("flipped");
+        card.style.pointerEvents = "none";
+    })
+    } else {
+        console.log("wrong");
+        flippedCards.forEach((card) => {
+            card.classList.remove("flipped");
+            setTimeout(() => card.classList.remove("toggleCard"), 1000);
+       });
+    }
+  }
+};
+}
+cardGenerator()
